3e10d2f3ec526d0b4bd33b94e66c00bba0c5e1b2
Saulo Curty <sullo152@gmail.com>
2022-01-26 21:46:16 -0300

    Resumo Tela Feita

--- ResumoDiario.py
+++ ResumoDiario.py
@@ -11,17 +11,21 @@ class ResumoDiario:
         master.title("Resumo")
         
         self.div1 = Frame(master, bg='#00CED1')
-        self.div1.grid(row=0, column=0)
+        self.div1.grid(row=0, column=0, pady=(30, 30))
         self.div2 = Frame(master, bg='#00CED1')
         self.div2.grid(row=1, column=0)
-        self.div3 = Frame(master)
+        self.div3 = Frame(master, bg='#00CED1')
         self.div3.grid(row=2, column=0)
 
 
-
-        self.titulo = Label(self.div1, font=('Arial', '16', 'bold'), text='Resumo Diario', bg='#00CED1')
-        self.titulo.grid(column=0, row=0)
-
+        self.botaoV = Button(self.div3, width=15, text='Voltar',relief=RAISED, bg='#5F9EA0')
+        self.botaoV.grid(column=0, row=2, padx=(30,0), pady=(30, 0))
+        self.titulo = Label(self.div1, font=('Arial', '20', 'bold'), text='Resumo Diario', bg='#00CED1')
+        self.titulo.grid(column=1, row=0)
+        self.titulo2 =Label(self.div2, font=('Arial', '16', 'bold'), text='Calorias Consumidas', bg='#00CED1')
+        self.titulo3 = Label(self.div2, font=('Arial', '16', 'bold'), text='Calorias Gastas', bg='#00CED1')
+        self.titulo2.grid(row=0, column=0)
+        self.titulo3.grid(row=0, column = 1)
         self.alimentosList = ttk.Treeview(self.div2,column=('nome', 'caloria', 'total'))
   
         self.alimentosList.heading("#0",text="")
@@ -30,27 +34,31 @@ class ResumoDiario:
         self.alimentosList.heading("total", text='Total')
 
         self.alimentosList.column("#0", width="0")
-        self.alimentosList.column("nome", width="150")
+        self.alimentosList.column("nome", width="130")
         self.alimentosList.column("caloria", width="70")
         self.alimentosList.column("total", width="70")
-        self.alimentosList.grid(row=1, column=0)
+        self.alimentosList.grid(row=1, column=0, padx=(22, 10))
 
         self.exerciciosList = ttk.Treeview(self.div2,column=('nome', 'caloria', 'total'))
-  
+
         self.exerciciosList.heading("#0",text="")
         self.exerciciosList.heading("nome",text="Nome")
         self.exerciciosList.heading("caloria", text='Calorias')
         self.exerciciosList.heading("total", text='Total')
 
         self.exerciciosList.column("#0", width="0")
-        self.exerciciosList.column("nome", width="150")
+        self.exerciciosList.column("nome", width="130")
         self.exerciciosList.column("caloria", width="70")
         self.exerciciosList.column("total", width="70")
-        self.exerciciosList.grid(row=1, column=1)
+        self.exerciciosList.grid(row=1, column=1, padx=(0, 5))
         
-        #self.exercicioList = ttk.Treeview(self.div2, height=3, column=())
+        self.recomenda = Label(self.div3, text='Recomendações', font=('Sans', '16', 'bold'), bg='#00CED1')
+        self.recomenda.grid(row=0, column=0, pady=(20, 10))
         
-        
+        self.recomenda_text = Message(self.div3, width=300, font=('sans', '14'), bg='#00CED1', justify=LEFT, fg='black')
+        self.recomenda_text['text'] = 'Iai meus gostoso, como estão? vão se alimentar karalho'
+
+        self.recomenda_text.grid(row=1, column=0)
 
     def Coluna_Alimento_Alimento(self, email: str):
         path_l = path_l = f'usuarios/{email}/Exercicio Dia.csv'
